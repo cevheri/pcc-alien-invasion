9b2b916efa892a405b21cfa04d6b5eeb4d07363e
cevher <cevheribozoglan@gmail.com>
2020-09-28 14:07:20 +0300

    modify to class

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ class Setting():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
 
+        # Ship settings
+        self.ship_speed_factor = 1.5
+
 
 #####################################################
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Load the ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
@@ -26,11 +30,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+
+        # Movement flags
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
+        """Update the ship's position based on movement flags."""
+
+        # Update the ship's center value, not the rect.
+
         if self.moving_right:
-            self.rect.centerx += 1
+            self.center += self.ai_settings.ship_speed_factor
+        # !!!!!!!!!!!!!!!!!!!!!!!!
+        if self.moving_left:
+            self.center -= self.ai_settings.ship_speed_factor
 
     def blitme(self):
         """Draw the ship at its current location."""
@@ -46,14 +62,18 @@ class GameFunctions():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            ###################################
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
                     ship.moving_right = True
                 elif event.key == pygame.K_LEFT:
-                    ship.rect.centerx -= 1
+                    ship.moving_left = True
+            ###################################
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT:
                     ship.moving_right = False
+                elif event.key == pygame.K_LEFT:
+                    ship.moving_left = False
 
     def update_screen(self, ai_settings, screen, ship):
         """Update images on the screen and flip to the new screen."""
@@ -76,7 +96,7 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
     gf = GameFunctions()
 
     while True:
