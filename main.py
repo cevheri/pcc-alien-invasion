b27d48ebed52e9f63e336b9e8cd7d7ad972ec98f
cevher <cevheribozoglan@gmail.com>
2020-09-28 12:20:55 +0300

    move to class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 
 
+#####################################################
 class Setting():
     def __init__(self):
         self.screen_width = 1200
@@ -9,6 +10,7 @@ class Setting():
         self.bg_color = (230, 230, 230)
 
 
+#####################################################
 class Ship():
 
     def __init__(self, screen):
@@ -24,11 +26,47 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        self.moving_right = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += 1
+
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
 
 
+#####################################################
+class GameFunctions():
+
+    def check_events(self, ship):
+        """Respond to keypresses and mouse events."""
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    ship.rect.centerx -= 1
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    ship.moving_right = False
+
+    def update_screen(self, ai_settings, screen, ship):
+        """Update images on the screen and flip to the new screen."""
+
+        # Redraw the screen during each pass through the loop.
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+#####################################################
 def run_game():
     # Initialize game and create a screen object.
     pygame.init()
@@ -38,18 +76,15 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    ship = Ship(screen)
+    gf = GameFunctions()
+
     while True:
+        gf.check_events(ship)
 
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        ship.update()
 
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.update_screen(ai_settings, screen, ship)
 
 
 run_game()
